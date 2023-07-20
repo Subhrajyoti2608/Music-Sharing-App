754a570ba595970643d0ecb2087a2b35880115d5
Subhrajyoti Mahato <86191223+Subhrajyoti2608@users.noreply.github.com>
2023-07-20 19:06:32 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -57,15 +57,22 @@ def stop():
     mixer.music.pause()
     infoLabel.configure(text="")
   
+def resume():
+   global song_selected
+   mixer.init()
+   mixer.music.load('shared_files/'+song_selected)
+   mixer.music.play()
 
+
+def pause():
+    global song_selected
+    pygame
+    mixer.init()
+    mixer.music.load('shared_files/'+song_selected)
+    mixer.music.pause()  
    
  
 
-
-
-
-    
-        
 #Client GUI
 def musicWindow(): 
     global song_counter
@@ -94,6 +101,12 @@ def musicWindow():
     
     PlayButton=Button(window,text="Play", width=10,bd=1,bg='SkyBlue',font = ("Calibri",10), command = play)
     PlayButton.place(x=30,y=200)
+
+    ResumeButton=Button(window,text="Resume", width=10,bd=1,bg='SkyBlue',font = ("Calibri",10), command = resume)
+    ResumeButton.place(x=30,y=250)
+
+    PauseButton=Button(window,text="Pause", width=10,bd=1,bg='SkyBlue',font = ("Calibri",10), command = pause)
+    PauseButton.place(x=200,y=250)
     
     Stop=Button(window,text="Stop",bd=1,width=10,bg='SkyBlue', font = ("Calibri",10), command = stop)
     Stop.place(x=200,y=200)
